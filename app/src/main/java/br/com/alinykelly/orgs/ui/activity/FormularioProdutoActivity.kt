73e5f3808472baee0d02613b39c5fa70a75e8cc8
Aliny Kelly <alinykellyfs@gmail.com>
2022-11-06 19:45:55 -0300

    Finalizando a integracao do fluxo do aplicativo

--- app/src/main/java/br/com/alinykelly/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alinykelly/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -42,6 +42,7 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
             val dao = ProdutosDao()
             dao.adiciona(produtoNovo)
             Log.i("FormularioProduto", "onCreate: ${dao.buscaTodos()}")
+            finish()
 
         }
 
