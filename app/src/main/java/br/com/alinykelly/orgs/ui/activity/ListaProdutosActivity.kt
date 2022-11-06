32b4372684aebee917a2e0e9f7d6331e4528ab36
Aliny Kelly <alinykellyfs@gmail.com>
2022-11-06 20:02:10 -0300

    refatorando o código, atualizando produtos com o adapter

--- app/src/main/java/br/com/alinykelly/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alinykelly/orgs/ui/activity/ListaProdutosActivity.kt
@@ -1,38 +1,50 @@
 package br.com.alinykelly.orgs.ui.activity
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alinykelly.orgs.R
 import br.com.alinykelly.orgs.dao.ProdutosDao
-import br.com.alinykelly.orgs.model.Produto
 import br.com.alinykelly.orgs.ui.recyclerView.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.math.BigDecimal
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
+
+    private val dao = ProdutosDao()
+    private val adapter = ListaProdutosAdapter(
+        context = this, produtos = dao.buscaTodos()
+    )
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        configuraRecyclerView()
+        configuraFab()
     }
 
     override fun onResume() {
         super.onResume()
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val dao = ProdutosDao()
-        recyclerView.adapter = ListaProdutosAdapter(
-            context = this, produtos = dao.buscaTodos()
-        )
+        adapter.atualiza(dao.buscaTodos())
+
+    }
+
+    private fun configuraFab() {
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         fab.setOnClickListener {
-            val intent = Intent(this, FormularioProdutoActivity::class.java)
-            startActivity(intent)
+            vaiParaFormularioProduto()
         }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter =adapter
     }
 
 }
