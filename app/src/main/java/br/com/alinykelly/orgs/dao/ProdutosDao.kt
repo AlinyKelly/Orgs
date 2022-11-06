42ee25394ba80af96db082a7a4ff7233716688a3
Aliny Kelly <alinykellyfs@gmail.com>
2022-11-06 18:51:59 -0300

    Criacao do DAO

---
+++ app/src/main/java/br/com/alinykelly/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,16 @@
+package br.com.alinykelly.orgs.dao
+
+import br.com.alinykelly.orgs.model.Produto
+
+class ProdutosDao {
+
+    private val produtos = mutableListOf<Produto>()
+
+    fun adiciona(produto: Produto){
+        produtos.add(produto)
+    }
+
+    fun buscaTodos() : List<Produto> {
+        return produtos.toList()
+    }
+}
