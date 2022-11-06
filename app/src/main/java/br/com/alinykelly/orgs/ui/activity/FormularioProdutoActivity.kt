63fe04b21cc1c9068dd319ea8ba6addba593ff7a
Aliny Kelly <alinykellyfs@gmail.com>
2022-11-06 17:52:16 -0300

    Configurando Listener do BotaoSalvar

--- app/src/main/java/br/com/alinykelly/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alinykelly/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,11 +2,23 @@ package br.com.alinykelly.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import br.com.alinykelly.orgs.R
 
-class FormularioProdutoActivity : AppCompatActivity() {
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_produto)
+
+        //Processo de binding utilizando lambda
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProduto", "onCreate: $nome")
+
+        }
+
     }
 }
